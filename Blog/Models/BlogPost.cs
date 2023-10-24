24927af51b922adcd5d42e324483e4e004d75fd4
Gele <gellehussen@gmail.com>
2023-10-24 19:01:28 +0200

    added domain class for blog posts

---
+++ Blog/Models/BlogPost.cs
@@ -0,0 +1,23 @@
+﻿using System;
+namespace Blog.Models
+{
+	public class BlogPost
+	{
+		public Guid ID { get; set; }
+		public string Heading { get; set; }
+		public string PageTitle { get; set; }
+        public string Content { get; set; }
+        public string Description { get; set; }
+        public string FeaturedImage { get; set; }
+        public string UrlHandle { get; set; }
+		public DateTime PublishedDate { get; set; }
+		public string Author { get; set; }
+		public bool Visibility { get; set; }
+
+
+		public BlogPost()
+		{
+		}
+	}
+}
+
