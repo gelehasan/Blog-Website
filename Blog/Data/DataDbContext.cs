3b2714cfd9d9f820ef0441f371b11ac9fcf2a2f9
Gele <gellehussen@gmail.com>
2023-10-28 21:43:59 +0200

    finilized setting up db context and tables

--- Blog/Data/DataDbContext.cs
+++ Blog/Data/DataDbContext.cs
@@ -1,13 +1,24 @@
 ﻿using System;
+using Blog.Models;
 using Microsoft.EntityFrameworkCore;
 
 namespace Blog.Data
 {
 	public class DataDbContext : DbContext
 	{
-		public DataDbContext()
+
+
+		public DataDbContext(DbContextOptions options): base (options)
 		{
+
 		}
+
+		//Many to many relationship
+		public DbSet<BlogPost> BlogTable { get; set; }
+
+		public DbSet<Tag> TagTable { get; set; }
 	}
+
 }
+
 
